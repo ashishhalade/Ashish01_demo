f9489d3331b6a30ce430f72f6ba95f7d9b9e60d9
ashishhalade <58586226+ashishhalade@users.noreply.github.com>
2019-12-06 13:00:34 +0530

    Create file.c

---
+++ file.c
@@ -0,0 +1,14 @@
+int sum(int,int);
+int main() {
+  int a=10, b=12, c=18, d;
+  d=sum(a,b,c); //??
+  printf("d=%d\n",d);
+  d=sum(a);
+  printf("d=%d\n",d);
+  return 0;
+}
+int sum(int x,int y) {
+  int z;
+  z = x + y;
+  return z;
+}
